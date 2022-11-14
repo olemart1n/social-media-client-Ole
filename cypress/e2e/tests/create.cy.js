c706bec9ef61b0e814a15e960d2c763ecd0f9da1
Ole M <olesnoen@gmail.com>
2022-11-14 21:00:40 +0100

    just another test

--- cypress/e2e/tests/create.cy.js
+++ cypress/e2e/tests/create.cy.js
@@ -1,7 +1,7 @@
 describe("Validate user input", () => {
   beforeEach(() => {
     cy.clearLocalStorage();
-    cy.visit("127.0.0.1:5500/");
+    cy.visit("127.0.0.1:8080/");
     cy.wait(1000);
     cy.get(".btn-close:visible").click({ force: true });
     cy.get("button[data-auth='login']:visible").click({ force: true });
